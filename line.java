f363c9f5174d188ec930f41cc92b74e66a903a6f
AdityaS98 <adityasharmalko1998@gmail.com>
2023-04-09 12:38:38 +0530

    Added: Program for Welcoming message

---
+++ line.java
@@ -0,0 +1,30 @@
+package linecomputation;
+
+import java.util.*;
+
+/**
+ * 
+ * @author DELL
+ *
+ */
+
+public class linecalculationUC1 {
+	/**
+	 * 
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+		/*
+		 * 1)Taking input for x coordinates 2)Taking Input for y coordinates
+		 * 3)Calculating length of line
+		 */
+		Scanner sc = new Scanner(System.in);
+		/*
+		 * Taking Input of X1 Coordinate.
+		 */
+		
+		System.out.println("Welcome to Line Comparison Computation Program");
+		}
+	}
+
